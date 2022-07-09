e67e84dd589c665c6ef574617378107d08ac501c
palsarip <naufal.syarif0809@gmail.com>
2022-07-09 13:11:14 +0700

    feat: adding signIn & signUp features and styling

--- pages/signIn/index.tsx
+++ pages/signIn/index.tsx
@@ -1,47 +1,71 @@
-import {
-    Flex, Text, Box, Button, HStack, Link, Select,
-    Input, InputGroup, InputLeftAddon, InputRightAddon, VStack, FormControl, FormLabel
-} from '@chakra-ui/react'
-import MxmIconSVG from '../../public/mxmIcon.svg'
-import Image from 'next/image'
-import {useRouter} from 'next/router'
-import {useForm} from 'react-hook-form'  
-import {useEffect, useState} from 'react'
+import { Flex, Text, Box, Button, HStack, Link, Select, Input, InputGroup, InputLeftAddon, InputRightAddon, InputLeftElement, VStack, FormControl, FormLabel, Heading, Center, Container } from "@chakra-ui/react";
+import MxmIconSVG from "../../public/mxmIcon.svg";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+import { RiAccountCircleLine } from "react-icons/ri";
+import { RiKey2Fill } from "react-icons/ri";
 
 const signIn = () => {
-    const { register, handleSubmit, formState:{errors} } = useForm()
-    const onSubmit = async (data)=>{}
-    return ( 
-        <>
-        <Flex border={'solid'} minH={'100vh'} justifyContent={'center'} alignItems={'center'} bgGradient={'linear(to-r, #bd0116, #f74f24)'}>
-            <Flex height={'500px'} width={'400px'} borderRadius={25} boxShadow={'2xl'} bgColor={'#fff'} justifyContent={'center'} alignItems={'center'}>
-                <VStack>
-                    <Flex border={'solid'} width={'400px'} px={5}>
-                        <HStack>
-                            <Image src={MxmIconSVG} width={'50px'} height={'50px'}/>
-                            <Text>MAXIMA 2022</Text>
-                        </HStack>
-                    </Flex>
-                    <Flex border={'solid'} width={'400px'} justifyContent={'center'} alignItems={'center'}><Text>Log In to Dashboard</Text></Flex>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <FormControl width={'350px'} onSubmit={handleSubmit(onSubmit)}>
-                            <FormLabel>Nomor Induk Mahasiswa</FormLabel>
-                            <Input {...register('nim', {required: "NIM harus diisi"})} placeholder='44898' type='text' name='nim' textColor={'black'} border={'solid'}/>
-                            {errors.nim !== undefined && <Text textColor={'red'}>{errors.nim.message}</Text>}
-                            <FormLabel>Password</FormLabel>
-                            <Input {...register('password', {required: "Password harus diisi"})} placeholder='********' type='password' name='password' textColor={'black'} border={'solid'}/>
-                            {errors.password !== undefined && <Text textColor={'red'}>{errors.password.message}</Text>}
-                        </FormControl>
-                        <Flex w={'100%'} justifyContent={'center'} py={3}>
-                            <Button borderRadius={'md'} width={'350px'} type="submit" fontFamily={'rubik'} color={'#fff'} colorScheme={'orange'}>Sign In</Button>
-                        </Flex>
-                    </form>
-                    <Flex border={'solid'} width={'400px'} justifyContent={'center'} alignItems={'center'}><Text>Don't Have an Account? <Link href='/signUp'><a>Register</a></Link></Text></Flex>
-                </VStack>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = async (data: any) => {};
+  return (
+    <>
+      <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} bgGradient={"linear(to-r, #bd0116, #f74f24)"}>
+        <Flex height={"100%"} padding={"2em 2.5em"} borderRadius={25} boxShadow={"lg"} bgColor={"#fff"} justifyContent={"center"} alignItems={"center"}>
+          <Box>
+            <HStack>
+              <Image src={MxmIconSVG} width={"20px"} height={"20px"} />
+              <Text color={"#FF6B6B"} fontWeight={"bold"}>
+                MAXIMA 2022
+              </Text>
+            </HStack>
+            <Center>
+              <Heading as="h3" size="lg" color={"#5E81F4"} my={"0.5em"}>
+                Log In to Dashboard
+              </Heading>
+            </Center>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <FormControl onSubmit={handleSubmit(onSubmit)}>
+                <FormLabel fontFamily="rubik">Nomor Induk Mahasiswa</FormLabel>
+                <InputGroup>
+                  <InputLeftElement pointerEvents="none" children={<RiAccountCircleLine color="black" />} />
+                  <Input {...register("nim", { required: "NIM harus diisi" })} placeholder="44898" type="text" name="nim" textColor={"black"} border={"solid"} />
+                </InputGroup>
+                {errors.nim !== undefined && <Text textColor={"red"}>{errors.nim.message}</Text>}
+                <FormLabel mt={"1em"} fontFamily="rubik">
+                  Password
+                </FormLabel>
+                <InputGroup>
+                  <InputLeftElement pointerEvents="none" children={<RiKey2Fill color="black" />} />
+                  <Input {...register("password", { required: "Password harus diisi" })} placeholder="****" type="password" name="password" textColor={"black"} border={"solid"} />
+                </InputGroup>
+                {errors.password !== undefined && <Text textColor={"red"}>{errors.password.message}</Text>}
+              </FormControl>
+              <Flex w={"100%"} justifyContent={"center"} py={3} mt={"0.5em"}>
+                <Button borderRadius={"md"} w={"full"} type="submit" fontFamily={"rubik"} color={"#fff"} colorScheme={"orange"} bgColor={"#FF855F"}>
+                  Sign In
+                </Button>
+              </Flex>
+            </form>
+            <Flex justifyContent={"center"} alignItems={"center"}>
+              <Text fontSize={"14px"} fontFamily="rubik" color={"gray.500"}>
+                Don't Have an Account?
+                <Link href="/signUp" color={"#FF855F"} fontFamily="rubik">
+                  <a> Register</a>
+                </Link>
+              </Text>
             </Flex>
+          </Box>
         </Flex>
-        </>
-     );
-}
- 
+      </Flex>
+    </>
+  );
+};
+
 export default signIn;
--- pages/signUp/index.tsx
+++ pages/signUp/index.tsx
@@ -1,40 +1,93 @@
-import {
-    Flex, Text, Box, Button, Image, Stack, Link, Select,
-    Input, InputGroup, InputLeftAddon, InputRightAddon, VStack, FormControl, FormLabel
-} from '@chakra-ui/react'
-import MxmIconSVG from '../../public/mxmIcon.svg'
-import {useRouter} from 'next/router'
-import {useForm} from 'react-hook-form'  
-import {useEffect, useState} from 'react'
+import { Flex, Text, Box, Button, HStack, Link, Select, Input, InputGroup, InputLeftAddon, InputRightAddon, InputLeftElement, VStack, FormControl, FormLabel, Heading, Center, Container } from "@chakra-ui/react";
+import MxmIconSVG from "../../public/mxmIcon.svg";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+import { RiAccountCircleLine } from "react-icons/ri";
+import { RiKey2Fill } from "react-icons/ri";
+import { MdDriveFileRenameOutline, MdOutlineAlternateEmail } from "react-icons/md";
 
-const signUp = () => {
-    const { register, handleSubmit, formState:{errors} } = useForm()
-    const onSubmit = async (data)=>{}
-    return ( 
-        <>
-        <Flex border={'solid'} minH={'100vh'} justifyContent={'center'} alignItems={'center'} bgGradient={'linear(to-r, #bd0116, #f74f24)'}>
-            <Flex height={'500px'} width={'400px'} borderRadius={25} boxShadow={'2xl'} bgColor={'#fff'} justifyContent={'center'} alignItems={'center'}>
-                <VStack>
-                    <Flex border={'solid'} width={'400px'}></Flex>
-                    <Flex border={'solid'} width={'400px'}></Flex>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <FormControl width={'350px'} onSubmit={handleSubmit(onSubmit)}>
-                            <FormLabel>Nomor Induk Mahasiswa</FormLabel>
-                            <Input {...register('nim', {required: "NIM harus diisi"})} type='text' name='nim' textColor={'black'} border={'solid'}/>
-                            {errors.nim !== undefined && <Text textColor={'red'}>{errors.nim.message}</Text>}
-                            <FormLabel>Password</FormLabel>
-                            <Input {...register('password', {required: "Password harus diisi"})} type='password' name='password' textColor={'black'} border={'solid'}/>
-                            {errors.password !== undefined && <Text textColor={'red'}>{errors.password.message}</Text>}
-                        </FormControl>
-                        <Flex w={'100%'} justifyContent={'center'} py={3}>
-                            <Button borderRadius={'md'} width={'350px'} type="submit" fontFamily={'rubik'}>SUBMIT</Button>
-                        </Flex>
-                    </form>
-                </VStack>
+const signIn = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = async (data: any) => {};
+  return (
+    <>
+      <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} bgGradient={"linear(to-r, #bd0116, #f74f24)"}>
+        <Flex height={"100%"} padding={["1.5em", "2em"]} m={["0em", "1em"]} borderRadius={25} boxShadow={"lg"} bgColor={"#fff"} justifyContent={"center"} alignItems={"center"}>
+          <Box>
+            <HStack>
+              <Image src={MxmIconSVG} width={"20px"} height={"20px"} />
+              <Text color={"#FF6B6B"} fontWeight={"bold"}>
+                MAXIMA 2022
+              </Text>
+            </HStack>
+            <Center>
+              <Heading as="h3" size="lg" color={"#5E81F4"} my={"0.5em"}>
+                Let's Create Your Account<span style={{ color: "#F16484" }}>!</span>
+              </Heading>
+            </Center>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <FormControl onSubmit={handleSubmit(onSubmit)}>
+                <FormLabel fontFamily="rubik">Nomor Induk Mahasiswa</FormLabel>
+                <InputGroup>
+                  <InputLeftElement pointerEvents="none" children={<RiAccountCircleLine color="black" />} />
+                  <Input {...register("nim", { required: "NIM harus diisi" })} placeholder="44898" type="text" name="nim" textColor={"black"} border={"solid"} />
+                </InputGroup>
+                {errors.nim !== undefined && <Text textColor={"red"}>{errors.nim.message}</Text>}
+                <FormLabel mt={"1em"} fontFamily="rubik">
+                  Nama Lengkap
+                </FormLabel>
+                <InputGroup>
+                  <InputLeftElement pointerEvents="none" children={<MdDriveFileRenameOutline color="black" />} />
+                  <Input {...register("namaLengkap", { required: "Nama lengkap harus diisi" })} placeholder="John Doe" type="text" name="namaLengkap" textColor={"black"} border={"solid"} />
+                </InputGroup>
+                {errors.namaLengkap !== undefined && <Text textColor={"red"}>{errors.namaLengkap.message}</Text>}
+                <FormLabel mt={"1em"} fontFamily="rubik">
+                  Email Student
+                </FormLabel>
+                <InputGroup>
+                  <InputLeftElement pointerEvents="none" children={<MdOutlineAlternateEmail color="black" />} />
+                  <Input {...register("email", { required: "Email harus diisi" })} placeholder="abc@student.umn.ac.id" type="text" name="email" textColor={"black"} border={"solid"} />
+                </InputGroup>
+                {errors.email !== undefined && <Text textColor={"red"}>{errors.email.message}</Text>}
+                <FormLabel mt={"1em"} fontFamily="rubik">
+                  Password
+                </FormLabel>
+                <InputGroup>
+                  <InputLeftElement pointerEvents="none" children={<RiKey2Fill color="black" />} />
+                  <Input {...register("password", { required: "Password harus diisi" })} placeholder="****" type="password" name="password" textColor={"black"} border={"solid"} />
+                </InputGroup>
+                {errors.password !== undefined && <Text textColor={"red"}>{errors.password.message}</Text>}
+                <FormLabel mt={"1em"} fontFamily="rubik">
+                  Divisi
+                </FormLabel>
+                <Input {...register("divisi", { required: "Divisi harus diisi" })} placeholder="Divisi 1" type="text" name="divisi" textColor={"black"} border={"solid"} />
+                {errors.divisi !== undefined && <Text textColor={"red"}>{errors.divisi.message}</Text>}
+              </FormControl>
+              <Flex w={"100%"} justifyContent={"center"} py={3} mt={"0.5em"}>
+                <Button borderRadius={"md"} w={"full"} type="submit" fontFamily={"rubik"} color={"#fff"} colorScheme={"orange"} bgColor={"#FF855F"}>
+                  Sign Up
+                </Button>
+              </Flex>
+            </form>
+            <Flex justifyContent={"center"} alignItems={"center"}>
+              <Text fontSize={"14px"} fontFamily="rubik" color={"gray.500"}>
+                Already a User?
+                <Link href="/signIn" color={"#FF855F"} fontFamily="rubik">
+                  <a> Login</a>
+                </Link>
+              </Text>
             </Flex>
+          </Box>
         </Flex>
-        </>
-     );
-}
- 
-export default signUp;
+      </Flex>
+    </>
+  );
+};
+
+export default signIn;
