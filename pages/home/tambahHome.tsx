3a545d336c63721e78bbd7e037fafcf7d6a926d3
palsarip <naufal.syarif0809@gmail.com>
2022-07-15 10:26:54 +0700

    feat: adding padding on form

--- pages/home/tambahHome.tsx
+++ pages/home/tambahHome.tsx
@@ -1,154 +1,110 @@
-import Sidebar from '../../components/Sidebar'
-import Navbar from '../../components/Navbar'
-import {
-  Box,
-  Flex,
-  Text,
-  Avatar,
-  Img,
-  FormControl,
-  FormLabel,
-  Input,
-  Button,
-  Select,
-  Textarea
-} from '@chakra-ui/react'
-import { HStack } from '@chakra-ui/react';
-import Link from 'next/link';
-import Dropzone from 'react-dropzone';
-import MxmIconSVG from '../../public/mxmIcon.svg'
-import Image from 'next/image'
-import { useForm } from 'react-hook-form'
-import { useState } from 'react'
+import Sidebar from "../../components/Sidebar";
+import Navbar from "../../components/Navbar";
+import { Box, Flex, Text, Avatar, Img, FormControl, FormLabel, Input, Button, Select, Textarea } from "@chakra-ui/react";
+import { HStack } from "@chakra-ui/react";
+import Link from "next/link";
+import Dropzone from "react-dropzone";
+import MxmIconSVG from "../../public/mxmIcon.svg";
+import Image from "next/image";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 const tambahHome = () => {
-  const [divisi, setDivisi] = useState([])
-  const [isButtonLoading, setIsButtonLoading] = useState(false)
-  const [error, setError] = useState(undefined)
+  const [divisi, setDivisi] = useState([]);
+  const [isButtonLoading, setIsButtonLoading] = useState(false);
+  const [error, setError] = useState(undefined);
   const onSubmit = async (data: any) => {
-    console.log(data)
-    try{
-      setIsButtonLoading(true)
+    console.log(data);
+    try {
+      setIsButtonLoading(true);
       setTimeout(async () => {
-        setIsButtonLoading(false)
-      }, 3000)
-    } catch(err){
-        console.log(err.response.data.message)
-        setError(err.response.data.message)
-        setTimeout(async () => {
-          setIsButtonLoading(false)
-        }, 3000)
+        setIsButtonLoading(false);
+      }, 3000);
+    } catch (err: any) {
+      console.log(err.response.data.message);
+      setError(err.response.data.message);
+      setTimeout(async () => {
+        setIsButtonLoading(false);
+      }, 3000);
     }
   };
-  const { register, handleSubmit, formState:{errors} } = useForm()
-  return ( 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  return (
     <>
-      <Navbar/>
-      <Sidebar/>
-      <Flex
-        minH="100vh" 
-        bg={'#dee1e6'} 
-        ml={{base: 0, lg: '240px'}}
-        px={5}
-        pt={'75px'}
-        direction={'column'}
-        alignItems={'center'}
-        justifyContent={'center'}
-      >
-        <Box
-          w={'full'}
-          bgColor={'white'}
-          borderRadius={20}
-          mb={4}
-        >
-          <Flex
-            justifyContent={'space-between'}
-            alignItems={'center'}
-            mx={4}
-            borderBottom={'solid black'}
-          >
-            <Text 
-              fontSize={["15px", "25px", "25px", "25px"]} 
-              fontFamily="rubik"
-              fontWeight={600}
-              textColor={'black'}
-            >
+      <Navbar />
+      <Sidebar />
+      <Flex minH="100vh" bg={"#dee1e6"} ml={{ base: 0, lg: "240px" }} px={5} pt={"75px"} direction={"column"} alignItems={"center"} justifyContent={"center"}>
+        <Box w={"full"} bgColor={"white"} borderRadius={20} mb={4}>
+          <Flex justifyContent={"space-between"} alignItems={"center"} mx={4} borderBottom={"solid black"}>
+            <Text fontSize={["15px", "25px", "25px", "25px"]} fontFamily="rubik" fontWeight={600} textColor={"black"}>
               Tambah HoME
             </Text>
-            <Flex p={'10px'}>
-              <Image src={MxmIconSVG} width={'50px'} height={'50px'}/>
+            <Flex p={"10px"}>
+              <Image src={MxmIconSVG} width={"50px"} height={"50px"} />
             </Flex>
           </Flex>
-          <Box
-            py={4}
-            mx={4}
-          >
+          <Box py={4} mx={4}>
             <form onSubmit={handleSubmit(onSubmit)}>
-              <Flex justifyContent={'space-between'} mt={2} flexDirection={['column', 'column', 'row', 'row']}>
-                  <Box width={'100%'} px={2}>
-                    <FormLabel textColor={'black'}>Nama Organisator</FormLabel>
-                    <Input {...register('nama_organisator', {required: "Nama organisator harap diisi"})} type={'text'} name='nama_organisator' textColor={'black'} border={'solid'}/>
-                    {errors.nama_organisator !== undefined && <Text textColor={'red'}>{errors.nama_organisator.message}</Text>}
-                  </Box>
-                  <Box width={'100%'} px={2} mt={[2, 2, 0, 0]}>
-                    <FormLabel textColor={'black'}>Kategori</FormLabel>
-                    <Select {...register('kategori', {required: "Kategori harap dipilih"})} name='kategori' textColor={'black'} border={'solid'}/>
-                    {errors.kategori !== undefined && <Text textColor={'red'}>{errors.kategori.message}</Text>}
-                  </Box>
+              <Flex justifyContent={"space-between"} mt={2} mb={"0.8em"} flexDirection={["column", "column", "row", "row"]}>
+                <Box width={"100%"} px={2}>
+                  <FormLabel textColor={"black"}>Nama Organisator</FormLabel>
+                  <Input {...register("nama_organisator", { required: "Nama organisator harap diisi" })} type={"text"} name="nama_organisator" textColor={"black"} border={"solid"} />
+                  {errors.nama_organisator !== undefined && <Text textColor={"red"}>{errors.nama_organisator.message}</Text>}
+                </Box>
+                <Box width={"100%"} px={2} mt={[2, 2, 0, 0]}>
+                  <FormLabel textColor={"black"}>Kategori</FormLabel>
+                  <Select {...register("kategori", { required: "Kategori harap dipilih" })} name="kategori" textColor={"black"} border={"solid"} />
+                  {errors.kategori !== undefined && <Text textColor={"red"}>{errors.kategori.message}</Text>}
+                </Box>
               </Flex>
-              <Box width={'100%'} px={2}>
-                <FormLabel textColor={'black'}>Narasi Pendek</FormLabel>
-                <Input {...register('narasi_pendek', {required: "Narasi pendek harap diisi"})} name='narasi_pendek' textColor={'black'} border={'solid'}/>
-                {errors.narasi_pendek !== undefined && <Text textColor={'red'}>{errors.narasi_pendek.message}</Text>}
+              <Box width={"100%"} px={2} mb={"0.8em"}>
+                <FormLabel textColor={"black"}>Narasi Pendek</FormLabel>
+                <Input {...register("narasi_pendek", { required: "Narasi pendek harap diisi" })} name="narasi_pendek" textColor={"black"} border={"solid"} />
+                {errors.narasi_pendek !== undefined && <Text textColor={"red"}>{errors.narasi_pendek.message}</Text>}
               </Box>
-              <Box width={'100%'} px={2} mt={[2, 2, 0, 0]}>
-                <FormLabel textColor={'black'} htmlFor='no_hp'>Narasi Panjang</FormLabel>
-                <Textarea {...register('narasi_panjang', {required: "Narasi panjang harap diisi"})} name='narasi_panjang' textColor={'black'} border={'solid'}/>
-                {errors.narasi_panjang !== undefined && <Text textColor={'red'}>{errors.narasi_panjang.message}</Text>}
+              <Box width={"100%"} px={2} mt={[2, 2, 0, 0]} mb={"0.8em"}>
+                <FormLabel textColor={"black"} htmlFor="no_hp">
+                  Narasi Panjang
+                </FormLabel>
+                <Textarea {...register("narasi_panjang", { required: "Narasi panjang harap diisi" })} name="narasi_panjang" textColor={"black"} border={"solid"} />
+                {errors.narasi_panjang !== undefined && <Text textColor={"red"}>{errors.narasi_panjang.message}</Text>}
               </Box>
-              <Box width={'100%'} px={2} mt={[2, 2, 0, 0]}>
+              <Box width={"100%"} px={2} mt={[2, 2, 0, 0]} mb={"0.8em"}>
                 <FormLabel>Link Logo</FormLabel>
-                <Input {...register('link_logo', {required: "Link logo harap diisi"})} name='link_logo' textColor={'black'} border={'solid'}/>
-                {errors.link_logo !== undefined && <Text textColor={'red'}>{errors.link_logo.message}</Text>}
+                <Input {...register("link_logo", { required: "Link logo harap diisi" })} name="link_logo" textColor={"black"} border={"solid"} />
+                {errors.link_logo !== undefined && <Text textColor={"red"}>{errors.link_logo.message}</Text>}
               </Box>
-              <Box width={'100%'} px={2} mt={[2, 2, 0, 0]}>
+              <Box width={"100%"} px={2} mt={[2, 2, 0, 0]} mb={"0.8em"}>
                 <FormLabel>Link Video YouTube</FormLabel>
-                <Input {...register('link_yt', {required: "Link video YouTube harap diisi"})} name='link_yt' textColor={'black'} border={'solid'}/>
-                {errors.link_yt !== undefined && <Text textColor={'red'}>{errors.link_yt.message}</Text>}
+                <Input {...register("link_yt", { required: "Link video YouTube harap diisi" })} name="link_yt" textColor={"black"} border={"solid"} />
+                {errors.link_yt !== undefined && <Text textColor={"red"}>{errors.link_yt.message}</Text>}
               </Box>
-              <Flex justifyContent={'space-between'} mt={2} flexDirection={['column', 'column', 'row', 'row']}>
-                  <Box width={'100%'} px={2}>
-                    <FormLabel textColor={'black'}>Media Sosial (LINE)</FormLabel>
-                    <Input {...register('line', {required: "Media sosial (LINE) harap diisi"})} name='line' textColor={'black'} border={'solid'}/>
-                    {errors.line !== undefined && <Text textColor={'red'}>{errors.line.message}</Text>}
-                  </Box>
-                  <Box width={'100%'} px={2} mt={[2, 2, 0, 0]}>
-                    <FormLabel textColor={'black'}>Media Sosial (Instagram)</FormLabel>
-                    <Input {...register('instagram', {required: "Media sosial (Instagram) harap diisi"})} name='instagram' textColor={'black'} placeholder='Tidak Perlu Menggunakan @' border={'solid'}/>
-                    {errors.instagram !== undefined && <Text textColor={'red'}>{errors.instagram.message}</Text>}
-                  </Box>
+              <Flex justifyContent={"space-between"} mt={2} mb={"0.8em"} flexDirection={["column", "column", "row", "row"]}>
+                <Box width={"100%"} px={2}>
+                  <FormLabel textColor={"black"}>Media Sosial (LINE)</FormLabel>
+                  <Input {...register("line", { required: "Media sosial (LINE) harap diisi" })} name="line" textColor={"black"} border={"solid"} />
+                  {errors.line !== undefined && <Text textColor={"red"}>{errors.line.message}</Text>}
+                </Box>
+                <Box width={"100%"} px={2} mt={[2, 2, 0, 0]}>
+                  <FormLabel textColor={"black"}>Media Sosial (Instagram)</FormLabel>
+                  <Input {...register("instagram", { required: "Media sosial (Instagram) harap diisi" })} name="instagram" textColor={"black"} placeholder="Tidak Perlu Menggunakan @" border={"solid"} />
+                  {errors.instagram !== undefined && <Text textColor={"red"}>{errors.instagram.message}</Text>}
+                </Box>
               </Flex>
-              <Flex width={'100%'} px={2} mt={2} justifyContent={'right'}>
-                {isButtonLoading === true ? 
-                <Button 
-                isLoading 
-                w={100} 
-                borderRadius={'999px'} 
-                type="submit" textColor='black' 
-                bgColor={'green.200'} 
-                _hover={{bgColor: "yellow.200"}}>
-                  SUBMIT
-                </Button> 
-                : 
-                <Button 
-                w={100} 
-                borderRadius={'999px'} 
-                type="submit" 
-                textColor='black'
-                bgColor={'green.200'} 
-                _hover={{bgColor: "yellow.200"}}>
-                  SUBMIT
-                </Button>}
+              <Flex width={"100%"} px={2} mt={2} mb={"0.8em"} justifyContent={"right"}>
+                {isButtonLoading === true ? (
+                  <Button isLoading w={100} borderRadius={"999px"} type="submit" textColor="black" bgColor={"green.200"} _hover={{ bgColor: "yellow.200" }}>
+                    SUBMIT
+                  </Button>
+                ) : (
+                  <Button w={100} borderRadius={"999px"} type="submit" textColor="black" bgColor={"green.200"} _hover={{ bgColor: "yellow.200" }}>
+                    SUBMIT
+                  </Button>
+                )}
               </Flex>
             </form>
           </Box>
@@ -156,6 +112,6 @@ const tambahHome = () => {
       </Flex>
     </>
   );
-}
- 
+};
+
 export default tambahHome;
--- pages/home/tambahMediaHome.tsx
+++ pages/home/tambahMediaHome.tsx
@@ -1,101 +1,78 @@
-import Sidebar from '../../components/Sidebar'
-import Navbar from '../../components/Navbar'
-import {
-  Box,
-  Flex,
-  Text,
-  Avatar,
-  FormControl,
-  FormLabel,
-  Input,
-  Button,
-  Select
-} from '@chakra-ui/react'
-import MUIDataTable, {MUIDataTableColumn} from "mui-datatables";
-import { TableCell } from '@material-ui/core';
-import { VStack } from '@chakra-ui/react';
-import MxmIconSVG from '../../public/mxmIcon.svg'
-import Image from 'next/image'
-import { useForm } from 'react-hook-form'
-import { useState } from 'react'
+import Sidebar from "../../components/Sidebar";
+import Navbar from "../../components/Navbar";
+import { Box, Flex, Text, Avatar, FormControl, FormLabel, Input, Button, Select } from "@chakra-ui/react";
+import MUIDataTable, { MUIDataTableColumn } from "mui-datatables";
+import { TableCell } from "@material-ui/core";
+import { VStack } from "@chakra-ui/react";
+import MxmIconSVG from "../../public/mxmIcon.svg";
+import Image from "next/image";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 const tambahMedia = () => {
-  const [divisi, setDivisi] = useState([])
-  const [isButtonLoading, setIsButtonLoading] = useState(false)
-  const [error, setError] = useState(undefined)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const [divisi, setDivisi] = useState([]);
+  const [isButtonLoading, setIsButtonLoading] = useState(false);
+  const [error, setError] = useState(undefined);
+
   const onSubmit = async (data: any) => {
-    console.log(data)
-    try{
-      setIsButtonLoading(true)
+    console.log(data);
+    try {
+      setIsButtonLoading(true);
       setTimeout(async () => {
-        setIsButtonLoading(false)
-      }, 3000)
-    } catch(err){
-        console.log(err.response.data.message)
-        setError(err.response.data.message)
-        setTimeout(async () => {
-          setIsButtonLoading(false)
-        }, 3000)
+        setIsButtonLoading(false);
+      }, 3000);
+    } catch (err: any) {
+      console.log(err.response.data.message);
+      setError(err.response.data.message);
+      setTimeout(async () => {
+        setIsButtonLoading(false);
+      }, 3000);
     }
   };
-  const { register, handleSubmit, formState:{errors} } = useForm()
-  return ( 
+  return (
     <>
-      <Navbar/>
-      <Sidebar/>
-      <Flex
-        minH="100vh" 
-        bg={'#dee1e6'} 
-        ml={{base: 0, lg: '240px'}}
-        px={5}
-        pt={'75px'}
-        direction={'column'}
-        alignItems={'center'}
-        justifyContent={'center'}
-      >
-        <Box
-          w={'full'}
-          bgColor={'white'}
-          borderRadius={20}
-          mb={4}
-        >
-          <Flex
-            justifyContent={'space-between'}
-            alignItems={'center'}
-            mx={4}
-            borderBottom={'solid black'}
-          >
-            <Text 
-              fontSize={["15px", "25px", "25px", "25px"]} 
-              fontFamily="rubik"
-              fontWeight={600}
-              textColor={'black'}
-            >
+      <Navbar />
+      <Sidebar />
+      <Flex minH="100vh" bg={"#dee1e6"} ml={{ base: 0, lg: "240px" }} px={5} pt={"75px"} direction={"column"} alignItems={"center"} justifyContent={"center"}>
+        <Box w={"full"} bgColor={"white"} borderRadius={20} mb={4}>
+          <Flex justifyContent={"space-between"} alignItems={"center"} mx={4} borderBottom={"solid black"}>
+            <Text fontSize={["15px", "25px", "25px", "25px"]} fontFamily="rubik" fontWeight={600} textColor={"black"}>
               Tambah Media HoME
             </Text>
-            <Flex p={'10px'}>
-              <Image src={MxmIconSVG} width={'50px'} height={'50px'}/>
+            <Flex p={"10px"}>
+              <Image src={MxmIconSVG} width={"50px"} height={"50px"} />
             </Flex>
           </Flex>
-          <Box
-            py={4}
-            mx={4}
-          >
+          <Box py={4} mx={4}>
             <form onSubmit={handleSubmit(onSubmit)}>
               <FormControl onSubmit={handleSubmit(onSubmit)}>
-                <FormLabel>Akun Organisator</FormLabel>
-                <Select {...register('akun_organisator', {required: "Akun organisator harap dipilih"})} name='akun_organisator' textColor={'black'} border={'solid'}/>
-                {errors.akun_organisator !== undefined && <Text textColor={'red'}>{errors.akun_organisator.message}</Text>}
-                <FormLabel>Link Media</FormLabel>
-                <Input {...register('link_media', {required: "Link media harap diisi"})} name='link_media' textColor={'black'} border={'solid'}/>
-                {errors.link_media !== undefined && <Text textColor={'red'}>{errors.link_media.message}</Text>}
+                <Box mb={"0.8em"}>
+                  <FormLabel>Akun Organisator</FormLabel>
+                  <Select {...register("akun_organisator", { required: "Akun organisator harap dipilih" })} name="akun_organisator" textColor={"black"} border={"solid"} />
+                  {errors.akun_organisator !== undefined && <Text textColor={"red"}>{errors.akun_organisator.message}</Text>}
+                </Box>
+                <Box>
+                  <FormLabel>Link Media</FormLabel>
+                  <Input {...register("link_media", { required: "Link media harap diisi" })} name="link_media" textColor={"black"} border={"solid"} />
+                  {errors.link_media !== undefined && <Text textColor={"red"}>{errors.link_media.message}</Text>}
+                </Box>
               </FormControl>
-              <Flex w={'100%'} justifyContent={'right'} py={3}>
-                {isButtonLoading === true ? 
-                  <Button isLoading borderRadius={'999px'} type="submit" fontFamily={'rubik'}>SUBMIT</Button> 
-                  : 
-                  <Button borderRadius={'999px'} type="submit" fontFamily={'rubik'}>SUBMIT</Button>
-                }
+              <Flex w={"100%"} justifyContent={"right"} py={3}>
+                {isButtonLoading === true ? (
+                  <Button isLoading borderRadius={"999px"} type="submit" fontFamily={"rubik"}>
+                    SUBMIT
+                  </Button>
+                ) : (
+                  <Button borderRadius={"999px"} type="submit" fontFamily={"rubik"}>
+                    SUBMIT
+                  </Button>
+                )}
               </Flex>
             </form>
           </Box>
@@ -103,6 +80,6 @@ const tambahMedia = () => {
       </Flex>
     </>
   );
-}
- 
+};
+
 export default tambahMedia;
